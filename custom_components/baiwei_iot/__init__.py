f2f835164de651e35a289cebda064c0980bbd909
Paul Lee <cn.paullee@gmail.com>
2025-05-28 00:45:26 +0800

    完成rooms映射为area业务

--- custom_components/baiwei_iot/__init__.py
+++ custom_components/baiwei_iot/__init__.py
@@ -1,9 +1,14 @@
+import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.area_registry import async_get as async_get_area_registry
 
 from .gateway.client import GatewayClient
 from .const import DOMAIN, PLATFORMS
 
+
+logger = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     serial_number = entry.data.get("serial_number")
@@ -17,17 +22,28 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await client.user_service.login()
 
     await client.device_service.get_devices_from_gateway()
-    await client.room_service.get_rooms()
+
+
+
     await client.scene_service.get_scenes()
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = client
 
+    # setup areas
+    rooms = await client.room_service.fetch_rooms()
+    area_registry = async_get_area_registry(hass)
+    for room in rooms:
+        area_registry.async_get_or_create(room.get("name"))
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
 
+
+
+
     return True
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unloaded = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
--- custom_components/baiwei_iot/gateway/services/room.py
+++ custom_components/baiwei_iot/gateway/services/room.py
@@ -1,11 +1,21 @@
+import logging
+
 from .gateway import GatewayService
 
+logger = logging.getLogger(__name__)
 
 class RoomService:
     def __init__(self, gateway_service: GatewayService):
         self.gateway_service = gateway_service
-        self._rooms = {}
+        self._room_map = {}
 
-    async def get_rooms(self):
-        rooms = await self.gateway_service.send("room_mgmt/room_query", {})
-        self._rooms = rooms
+    async def fetch_rooms(self):
+        res = await self.gateway_service.send("room_mgmt/room_query", {})
+        rooms = res.get("room_list")
+        self._room_map = {room["id"]: room["name"] for room in rooms}
+
+        return rooms
+
+
+    async def get_room_name(self, room_id: int):
+        self._room_map.get(room_id, "")
